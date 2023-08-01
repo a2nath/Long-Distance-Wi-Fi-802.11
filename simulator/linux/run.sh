07b9ae57dbaf934bc4c1e44546ae2141aa969612
Abhimanyu <abhimanyu4@gmail.com>
2023-08-01 12:20:13 -0700

    Run script for linux build. Logging needs work

--- simulator/linux/run.sh
+++ simulator/linux/run.sh
@@ -1,13 +1,16 @@
 #!/bin/sh
 
+CURRPWD=$(pwd) # current dir
+OUT=$CURRPWD/model.out
+LOG=$CURRPWD/last_compile.log
 
 cd ../source/src
 
 echo -n "Compiling "
 if [ "$1" = "-release" ]; then
 	echo "release build...\n"
-	g++ Common.cpp Frames.cpp GUI.cpp Program.cpp Station_main.cpp Station_timers.cpp Timers.cpp TrafficGen.cpp WiFi_Main.cpp -std=c++11 -Wall | tee last_run.log
+	g++ Common.cpp Frames.cpp GUI.cpp Program.cpp Station_main.cpp Station_timers.cpp Timers.cpp TrafficGen.cpp WiFi_Main.cpp -o $OUT -std=c++11 -Wall | tee $LOG
 else
 	echo "debug build...\n"
-	g++ Common.cpp Frames.cpp GUI.cpp Program.cpp Station_main.cpp Station_timers.cpp Timers.cpp TrafficGen.cpp WiFi_Main.cpp -std=c++11 -Wall -g | tee last_run.log
+	g++ Common.cpp Frames.cpp GUI.cpp Program.cpp Station_main.cpp Station_timers.cpp Timers.cpp TrafficGen.cpp WiFi_Main.cpp -o $OUT -std=c++11 -Wall -g | tee $LOG
 fi
