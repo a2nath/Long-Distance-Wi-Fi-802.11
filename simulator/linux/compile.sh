0f30fd28897270963f059370180e1a0e4ef2244f
Abhimanyu <abhimanyu4@gmail.com>
2023-08-04 22:34:01 -0700

    change the compile script to enable the compiler macro flags functionality

--- simulator/linux/compile.sh
+++ simulator/linux/compile.sh
@@ -4,13 +4,39 @@ CURRPWD=$(pwd) # current dir
 OUT=$CURRPWD/model.out
 LOG=$CURRPWD/last_compile.log
 
+rm $OUT
 cd ../source/src
+files="Common.cpp Frames.cpp GUI.cpp Program.cpp Station_main.cpp Station_timers.cpp Timers.cpp TrafficGen.cpp WiFi_Main.cpp"
+#options="-ftime-report"
+#options="-pass-exit-codes"
+macro=""
+macro=$macro" -D DETERMINISTIC"
+#macro=$macro" -D SHOWGUI"
 
 echo -n "Compiling "
+start=`date +%s`
+
 if [ "$1" = "-release" ]; then
 	echo "release build...\n"
-	g++ Common.cpp Frames.cpp GUI.cpp Program.cpp Station_main.cpp Station_timers.cpp Timers.cpp TrafficGen.cpp WiFi_Main.cpp -o $OUT -std=c++11 -Wall | tee $LOG
+	g++ $files -o $OUT -std=c++11 $options $macro -Wall 2>&1 | tee $LOG
 else
 	echo "debug build...\n"
-	g++ Common.cpp Frames.cpp GUI.cpp Program.cpp Station_main.cpp Station_timers.cpp Timers.cpp TrafficGen.cpp WiFi_Main.cpp -o $OUT -std=c++11 -Wall -g | tee $LOG
+	g++ $files -o $OUT -std=c++11 $options $macro -Wall -g 2>&1 | tee $LOG
+fi
+
+end=`date +%s`
+
+# g++ exit code instead of using g++ flag
+gcc_exit_code=$(grep ": error:" $LOG)
+if [ -z "$gcc_exit_code" ]; then
+	exit_code=0
+else
+	exit_code=1
+fi
+
+# give confirmation of compilation
+if [ -f $OUT ]; then
+	echo "\n\n======================================================================================="
+	echo "program compiled on $(date) and took $(( $end - $start )) seconds with exit code $exit_code"
+	echo "======================================================================================="
 fi
